dcd1db4f826203cff23434b2a16af40df4000642
Thiago <thiagoutsch11@gmail.com>
2024-02-24 18:10:49 -0300

    feat: raiz, elevado, valor absoluto

--- src/aula004.java
+++ src/aula004.java
@@ -9,13 +9,13 @@ public class aula004 {
         B = Math.sqrt(y);
         C = Math.sqrt(25.0);
 
-        System.out.println("Raiz quadrada de " + x + " = " + A);
-        System.out.println("Raiz quadrada de " + y + " = " + B);
-        System.out.println("Raiz quadrada de 25 = " + C);
+        System.out.println("Raiz quadrada de " + x + " = " + A); //raiz quadrada de x é o resultado da equação A
+        System.out.println("Raiz quadrada de " + y + " = " + B);  //raiz quadrada de y é o resultado da equação B
+        System.out.println("Raiz quadrada de 25 = " + C);  
 
         A = Math.pow(x, y);
         B = Math.pow(x, 2.0);
-        C = Math.pow(5.0, 2.0);
+        C = Math.pow(5.0, 2.0); 
 
         System.out.println(x + " elevado a " + y + " = " + A);
         System.out.println(x + " elevado ao quadrado = " + B);
