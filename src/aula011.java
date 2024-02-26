60977b15bad5b6b5c4db705496ca5d6646ba1e72
Thiago <thiagoutsch11@gmail.com>
2024-02-26 18:17:48 -0300

    feat: bitwise

---
+++ src/aula011.java
@@ -0,0 +1,10 @@
+public class aula011 {
+    public static void main(String[] args) {
+        int n1 = 89;
+        int n2 = 60;
+
+        System.out.println(n1 & n2);
+        System.out.println(n1 | n2);
+        System.out.println(n1 ^ n2);
+    }
+}
