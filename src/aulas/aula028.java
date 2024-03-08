56806793bca81883617d63c4eb23f4b20cb400a7
Thiago <thiagoutsch11@gmail.com>
2024-03-08 19:30:20 -0300

    feat: aprofundando data-hota

---
+++ src/aulas/aula028.java
@@ -0,0 +1,32 @@
+package aulas;
+
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+
+public class aula028 {
+    public static void main(String[] args) {
+        LocalDate d04 = LocalDate.parse("2022-07-20");
+        LocalDateTime d05 = LocalDateTime.parse("2022-07-20T01:30:26");
+        Instant d06 = Instant.parse("2022-07-20T01:30:26Z");
+
+        DateTimeFormatter fmt1 = DateTimeFormatter.ofPattern("dd/MM/y");
+        DateTimeFormatter fmt2 = DateTimeFormatter.ofPattern("dd/MM/y HH:mm");
+        DateTimeFormatter fmt3 = DateTimeFormatter.ofPattern("dd/MM/y HH:mm").withZone(ZoneId.systemDefault());  //SÓ DA PRA FORMARTAR UMA DATA COM FUSO 
+                                                                                                                           // HORARIO (d06), SE TIVER O WITHZONE
+        DateTimeFormatter fmt4 = DateTimeFormatter.ISO_DATE_TIME;
+
+        System.out.println("d04 = " + d04.format(fmt1));
+        System.out.println("d04 = " + fmt1.format(d04));
+
+        System.out.println("d05 = " + d05.format(fmt1));
+        System.out.println("d05 = " + d05.format(fmt2));
+
+        System.out.println("d06 = " + fmt3.format(d06));
+
+        System.out.println("d05 = " + d05.format(fmt4));
+
+    }
+}
