9a1ef96ce60af6bd1164fe70b867f3cd24990184
Thiago <thiagoutsch11@gmail.com>
2024-03-18 18:02:14 -0300

    feat: stack trace

---
+++ src/aulas/aula037.java
@@ -0,0 +1,45 @@
+package aulas;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class aula037 {
+    public static void main(String[] args) {
+
+        method1();
+
+        System.out.println("End of program ");
+
+    }
+
+    public static void method1() {
+        System.out.println("**METHOD 1 START**");
+        method2();
+        System.out.println("**METHOD 1 END**");
+    }
+
+
+
+
+    public static void method2() {
+        System.out.println("**METHOD 2 START**");
+        Scanner sc = new Scanner(System.in);
+        
+        try {
+        String[] vect = sc.nextLine().split(" ");
+        int position = sc.nextInt();
+        System.out.println(vect[position]);
+        }
+        catch (ArrayIndexOutOfBoundsException e) {
+            System.err.println("Invalid position");
+            e.printStackTrace();
+            sc.next();
+        }
+        catch (InputMismatchException e) {
+            System.out.println("Input error");
+        }
+     
+        sc.close();
+        System.out.println("**METHOD 2 END**");
+    }
+}
